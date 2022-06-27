8d780a88cf5b3ac4a546caf58e199be1a0a116b7
Joshua_Burdick <31868836+Joshua-Burdick@users.noreply.github.com>
2022-06-27 13:22:05 -0400

    Add files via upload

---
+++ Time Lab/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time1 = new Time();
+            Console.Write("Time created with constructor that has no arguments.\n" +
+                $"Universal time is {time1.toUniversalString()}\n" +
+                $"Standard time is {time1.ToString()}\n\n");
+
+            Time time2 = new Time(3, 20, 20);
+            Console.Write("Time created with constructor that has 3 arguments.\n" +
+                $"Universal time is {time2.toUniversalString()}\n" +
+                $"Standard time is {time2.ToString()}\n\n");
+
+            Time time3 = new Time(13, 45);
+            Console.Write("Time created with constructor that has 2 arguments.\n" +
+                $"Universal time is {time3.toUniversalString()}\n" +
+                $"Standard time is {time3.ToString()}\n\n");
+
+            Time time4 = new Time(15);
+            Console.Write("Time created with constructor that has 1 arguments.\n" +
+                $"Universal time is {time4.toUniversalString()}\n" +
+                $"Standard time is {time4.ToString()}\n\n");
+
+
+            Console.WriteLine("Time created with constructor that has invalid values.");
+            try
+            {
+                Time time5 = new Time(25);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+            // Keep console open until user closes it
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Time Lab/Time.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Time
+    {
+        private int _hour, _minute, _second;
+        private DateTime time;
+
+        public int Hour
+        {
+            get { return _hour; }
+            set
+            {
+                Validate(value, 24, "Hour");
+                _hour = value;
+            }
+        }
+
+        public int Minute
+        {
+            get { return _minute; }
+            set
+            {
+                Validate(value, 60, "Minute");
+                _minute = value;
+            }
+        }
+
+        public int Second
+        {
+            get { return _second; }
+            set
+            {
+                Validate(value, 60, "Second");
+                _second = value;
+            }
+        }
+
+        public Time(int hour = 0, int minute = 0, int second = 0)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+            time = new DateTime(1, 1, 1, Hour, Minute, Second);
+        }
+
+        private void Validate(int var, int limit, string name)
+        {
+            if(var < 0 | var > limit)
+            {
+                throw new ArgumentOutOfRangeException($"{name} must be between 0 and {limit}");
+            }
+        }
+
+        public override string ToString()
+        {
+            string phase = "AM";
+
+            if(Hour > 12) { phase = "PM"; }
+
+            return time.ToString("hh:mm:ss") + " " + phase;
+        }
+
+        public string toUniversalString()
+        {
+            return time.ToString("HH:mm:ss");
+        }
+    }
+}
